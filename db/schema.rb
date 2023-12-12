87e91de947fa6f76d98593d04e0a14ff50170043
miloscole <27miloscole@gmail.com>
2023-12-12 22:38:31 +0100

    Add null false to customer fields, customer refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_07_193332) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_131014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
     t.string "phone"
     t.text "notes"
     t.datetime "created_at", null: false
