d5af16fdcc3bd9fd7ae78191d97c003d7fe82f2e
miloscole <27miloscole@gmail.com>
2024-02-14 19:03:32 +0100

    Reset-update password feature added, auth refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "welcome#index", via: :get
+
   resources :customers do
     member do
       get :delete
@@ -11,16 +13,18 @@ Rails.application.routes.draw do
     end
   end
 
-  root "welcome#index"
-
   namespace :authentication, path: "", as: "" do
-    resources :users, only: [:new, :create], path: "/register", path_names: { new: "/" }
-    resources :sessions, only: [:new, :create, :destroy], path: "/login", path_names: { new: "/" }
+    resources :users, only: [:new, :create], path: "/register", path_names: { new: "" }
+    resources :sessions, only: [:new, :create, :destroy], path: "/login", path_names: { new: "" }
   end
 
-  match "*path", to: "application#handle_404", via: :all
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
+  match "*path", to: "application#handle_404", via: :all
 end
